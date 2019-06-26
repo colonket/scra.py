5a95ee8c89f0871c7c3d92af5814ae9f4b94e6bf
colonEndBracket <ericiniguez59@gmail.com>
2019-06-26 09:38:04 -0500

    Commented on more blocks of code

--- scra.py
+++ scra.py
@@ -3,11 +3,13 @@ import glob, os, shutil, sys
 
 #scra.py - A File Scraping Python Script by colonEndBracket
 
+#If user only runs 'scra.py'
 if len(sys.argv) == 1:
     print("[About>] Recursive File Scraping from specified folder of specified file types into a folder labeled 'dump-scrapy'")
     print("[Usage>] ./scra.py <target_directory> <file_types>")
     print("[e.g. >] ./scra.py .. png jpg mp4")
     exit()
+#If user supplies directory but not file types
 elif len(sys.argv) == 2:
     print("[ERROR>] File types not listed")
     print("[Usage>] ./scra.py <target_directory> <file_types>")
@@ -18,14 +20,14 @@ elif len(sys.argv) == 2:
 os.system("rm -rf ./dump-scrapy")
 os.system("mkdir ./dump-scrapy")
 
-#Directories
+#Define directories
 user_home = os.path.expanduser("~")
 source_dir = sys.argv[1]
 if source_dir == user_home:
     source_dir += "/"
-
 dest_dir = "./dump-scrapy"
 
+#Process the file types
 filetypes = []
 typeprinted = "[scra.py>] Scraping for"
 for spectyp in sys.argv[2:]:
@@ -34,6 +36,7 @@ for spectyp in sys.argv[2:]:
 typeprinted += " files in "+source_dir
 print(typeprinted)
 
+#Find and copy files
 f_found = 0
 for i in filetypes:
     itech=source_dir+"**/*."+i
@@ -44,6 +47,7 @@ for i in filetypes:
             shutil.copy(file, dest_dir)
             f_found += 1
 
+#Print results
 if f_found == 0:
     print("[scra.py>] No files were scraped!")
 else:
